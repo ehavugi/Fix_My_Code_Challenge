9cc0650defe5759594811f1d7d7fb4c09db6d8c3
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-05-15 20:47:05 -0700

    fixed style errors

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -30,10 +30,10 @@ class square():
         """
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
 
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
-
